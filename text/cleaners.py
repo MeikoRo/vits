7f6aedd077d73338a2acf25d6b375a4ba8feef62
MeikoRo <118957641+MeikoRo@users.noreply.github.com>
2022-11-28 17:58:05 +0800

    Update cleaners.py

--- text/cleaners.py
+++ text/cleaners.py
@@ -4,9 +4,6 @@ from text.korean import latin_to_hangul, number_to_hangul, divide_hangul, korean
 from text.mandarin import number_to_chinese, chinese_to_bopomofo, latin_to_bopomofo, chinese_to_romaji, chinese_to_lazy_ipa, chinese_to_ipa, chinese_to_ipa2
 from text.sanskrit import devanagari_to_ipa
 from text.english import english_to_lazy_ipa, english_to_ipa2, english_to_lazy_ipa2
-from text.thai import num_to_thai, latin_to_thai
-from text.shanghainese import shanghainese_to_ipa
-from text.cantonese import cantonese_to_ipa
 from text.ngu_dialect import ngu_dialect_to_ipa
 
 
@@ -133,17 +130,6 @@ def cjke_cleaners2(text):
     return text
 
 
-def thai_cleaners(text):
-    text = num_to_thai(text)
-    text = latin_to_thai(text)
-    return text
-
-
-def shanghainese_cleaners(text):
-    text = shanghainese_to_ipa(text)
-    if re.match(r'[^\.,!\?\-…~]', text[-1]):
-        text += '.'
-    return text
 
 
 def chinese_dialect_cleaners(text):
